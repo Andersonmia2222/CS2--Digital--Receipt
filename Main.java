88f68860b657b6ce7a848c30a785d022beebdff2
Andersonmia2222 <mia.anderson2026@gbwl.org>
2025-10-24 12:54:54 +0000

    updated

--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
 public class Main {
+    public static void main(String[] args) {
 
-   public static void main(String []args) {
-      System.out.println("Hello World");
-   }
+String highSchoolName = "Birch Wathen Lenox High School";
+double drinkPrice = 1.50;
+double candyPrice = 1.25;
+double hotDogPrice = 2.75;
+double hamburgerPrice = 3.50;
+
+System.out.println("**************************************");
+System.out.println("*                                    *");
+System.out.println("*     " + highSchoolName + " Snack Bar *");
+System.out.println("*                                    *");
+System.out.println("*     Drink .......... $" + drinkPrice + "*");
+System.out.println("*     Candy .......... $" + candyPrice + "*");
+System.out.println("*     Hot Dog ........ $" + hotDogPrice + " *");
+System.out.println("*     Hamburger ...... $" + hamburgerPrice + *");
+System.out.println("*                                    *");
+System.out.println("**************************************");
+
+
+"Drink", qtyDrinks, drinkPrice, drinksTotal);
+
+
+    }
 }
